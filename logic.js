83931b7572ef11c29000b653dd69e3b6ab933b66
saltytomatoes <saltytomatoes1337@gmail.com>
2020-11-01 22:57:33 +0200

    menu ui fully designed

--- logic.js
+++ logic.js
@@ -2,6 +2,33 @@
 let body = document.body;
 
 
+function createInputDiv(description) {
+    let inputdiv = document.createElement("div");
+    inputdiv.classList.toggle("inputContainer");
+
+    let text = document.createElement("h2");
+    text.innerText = description;
+    inputdiv.appendChild(text);
+
+    let input = document.createElement("input");
+    input.setAttribute("type","text");
+    input.setAttribute("name",description);
+    inputdiv.appendChild(input);
+
+    return inputdiv;
+}
+
+
+function startClick() {
+    
+    let rows = document.querySelector(`input[name="rows"]`).value;
+    let cols = document.querySelector(`input[name="cols"]`).value;
+    
+    if(cols > 20 || cols < 3 || rows > 20 || rows < 3 || rows*cols % 2 != 0)
+        alert(`rows and cols has to be between 3 and 20 and cols*rows has to be even.`);
+}
+
+
 
 function menu() {
 
@@ -14,15 +41,14 @@ function menu() {
         container.appendChild(title);
 
 
-        let rowstxt = document.createElement("h2");
-        rowstxt.innerText = `rows`;
-        container.appendChild(rowstxt);
-
+        container.appendChild(createInputDiv("rows"));
+        container.appendChild( createInputDiv("cols") );
 
 
         let startBtn = document.createElement("button");
         startBtn.innerText = "start!";
         startBtn.setAttribute("id","startBtn");
+        startBtn.addEventListener("click",startClick);
         container.appendChild(startBtn);
 
     body.appendChild(container);
